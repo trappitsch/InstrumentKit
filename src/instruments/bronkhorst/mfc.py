07a8ef376f876939ab5ea6b6b7b365fc3677c43c
eve1374 <eve1374@yahoo.com>
2025-03-04 14:08:27 +0100

    alarm et io lecture ok

--- src/instruments/bronkhorst/mfc.py
+++ src/instruments/bronkhorst/mfc.py
@@ -26,12 +26,12 @@ class MFC(Instrument):
 
     Example:
         >>> import instruments as ik
-        >>> port = '/dev/ttyUSB0'
-        >>> baud = 15200
-        >>> inst = ik.comet.CitoPlus1310.open_serial(port, baud)
-        >>> inst.rf  # query RF state
-        False
-        >>> inst.rf = True  # turn on RF
+        >>> ip = '10.27.16.74'
+        >>> remote_port = 4001
+        >>> mfctest = ik.bronkhorst.MFC.open_tcpip(ip, remote_port)
+        >>> mfctest.name
+        'M23206772A'
+        >>> test.fsetpoint = 10 # set fsetpoint
     """
 
     def __init__(self, filelike, auth=None):
@@ -39,6 +39,39 @@ class MFC(Instrument):
         # initializing class requires an open serial port, that is in filelike
         # open_serial (or open_usb or open_...) returns a class correctly initialized
         super().__init__(filelike)
+        # commands definition
+        self._read_commands = {
+            "Serial number": ":0780047163716300\r\n",
+            "Temperature": ":06800421472147\r\n",
+            "Control Mode": ":06800401040104\r\n",
+            "Fsetpoint": ":06800421412143\r\n",
+            "Capacity unit": ":078004017F017F07\r\n",
+            "Counter unit": ":0780046867686704\r\n",
+            "Counter unit index": ":06800468026802\r\n",
+            "IO status": ":068004720B720B\r\n",
+            "Alarm info": ":06800401140114\r\n",
+        }
+        # Write commands expect a status
+        self._write_commands = {
+            "Control Mode": "80010104",
+            "Fsetpoint": "80012143",
+            "Capacity unit": "8001017F6D6C6E2F68\r\n",
+            "Counter unit index": "80016802",
+            "Counter unit": "8001686704",
+        }
+        self._data_type = {
+            "character": 0x00,
+            "integer": 0x20,
+            "float": 0x40,
+            "long": 0x40,
+            "string": 0x60,
+        }
+        # units as returned by register capacity unit, conversion to defined units
+        self._units = {
+            "mln/min": u.mlnpmin,
+            "sccm": u.sccm,
+        }
+
         self.auth = auth
         # error codes man p.13
         self._error_codes = {
@@ -89,37 +122,12 @@ class MFC(Instrument):
             "22": "Protocol error",
             "23": "Buffer overflow in module",
         }
-        self._data_type = {
-            "character": 0x00,
-            "integer": 0x20,
-            "float": 0x40,
-            "long": 0x40,
-            "string": 0x60,
-        }
-        # commands definition
-        # Set commands expect a status
-        self._commands = {
-            "Serial number": ":0780047163716300\r\n",
-            "Temperature": ":06800421472147\r\n",
-            "Control Mode": ":06800401040104\r\n",
-            "Set Control Mode": ":0580010104",
-            "Fsetpoint": ":06800421412143\r\n",
-            "Set fsetpoint": ":0880012143",
-            "Get capacity unit": ":078004017F017F07\r\n",
-        }
-        # units as returned by register capacity unit, conversion to defined units
-        self._units = {
-            "mln/min": u.mlnpmin,
-            "sccm": u.sccm,
-        }
 
     @property
     def name(self) -> str:
         """Get the serial number of the instrument."""
-        # decide what is the name to be the return
-        # put in docstring
 
-        data = self.query(self._commands.get("Serial number"))
+        data = self.query(self._read_commands.get("Serial number"))
         data = self._extract_data(data)
 
         return bytearray.fromhex(data).decode()
@@ -131,7 +139,7 @@ class MFC(Instrument):
         :rtype: Quantity
         """
 
-        data = self.query(self._commands.get("Temperature"))
+        data = self.query(self._read_commands.get("Temperature"))
         data = self._extract_data(data)
         temperature = struct.unpack("!f", bytes.fromhex(data))[0]
 
@@ -144,7 +152,7 @@ class MFC(Instrument):
         :rtype: Quantity
         """
 
-        data = self.query(self._commands.get("Fsetpoint"))
+        data = self.query(self._read_commands.get("Fsetpoint"))
         print("data raw : ", data)
         data = self._extract_data(data)
         print(data)
@@ -163,13 +171,11 @@ class MFC(Instrument):
             value = 0
             # * self.capacity_unit
         value = float(value.magnitude)
-        self.sendcmd(self._make_pkg(self._commands.get("Set fsetpoint"), value))
+        self.sendcmd(self._make_pkg(self._write_commands.get("Fsetpoint"), value))
         # read buffer and check status :
         # check dressler cesar 1312
         # todo error if reading setpoint back is very different
-        rep = self.query(self._commands.get("Fsetpoint"))
-        # todo handling errors
-        # print(self._unpack_status(reply))
+        rep = self.query(self._read_commands.get("Fsetpoint"))
 
     # todo return type ?
     @property
@@ -178,12 +184,118 @@ class MFC(Instrument):
         Get the unit of the flow measurement and setpoint,
         return it as a unit
         """
-        data = self.query(self._commands.get("Get capacity unit"))
-        print("capacity unit command : ", self._commands.get("Get capacity unit"))
+        data = self.query(self._read_commands.get("Capacity unit"))
+        print("capacity unit command : ", self._read_commands.get("Capacity unit"))
         print("capacity unit rawdata : ", data)
         data = self._extract_data(data)
         unit_str = bytearray.fromhex(data).decode()
         return self._units.get(unit_str)
+
+    @property
+    def counter_unit(self):
+        """Get counter unit"""
+        data = self.query(self._read_commands.get("Counter unit"))
+        print("counter unit rawdata : ", data)
+        data = self._extract_data(data)
+        unit_str = bytearray.fromhex(data).decode()
+        print("counter unit : ", unit_str)
+        return unit_str
+
+    @counter_unit.setter
+    def counter_unit(self, counter_unit):
+        self.sendcmd(
+            self._make_pkg(self._write_commands.get("Counter unit"), counter_unit)
+        )
+        # read buffer and check status :
+        # check dressler cesar 1312
+        # todo error if reading counter unit back is very different
+        rep = self.query(self._read_commands.get("Counter unit"))
+
+    @property
+    def counter_unit_index(self):
+        """Get counter unit index"""
+        data = self.query(self._read_commands.get("Counter unit index"))
+        print("counter unit index rawdata : ", data)
+        data = self._extract_data(data)
+        unit_str = bytearray.fromhex(data).decode()
+        print("counter unit : ", unit_str)
+        return unit_str
+
+    @counter_unit_index.setter
+    def counter_unit_index(self, counter_unit_index):
+        self.sendcmd(
+            self._make_pkg(
+                self._write_commands.get("Counter unit index"), counter_unit_index
+            )
+        )
+        # read buffer and check status :
+        # check dressler cesar 1312
+        # todo error if reading counter unit back is very different
+        rep = self.query(self._read_commands.get("Counter unit index"))
+
+    @property
+    def io_status(self):
+        """
+        Get all bits of IOstatus.
+        """
+        data = self.query(self._read_commands.get("IO status"))
+        data = self._extract_data(data)
+        print(data)
+        data = int(data, 16)
+        iostatus = [
+            "true = read special purpose jumper",
+            "not used",
+            "true = read analog mode jumper",
+            "true = read micro switch",
+            "special purpose jumper off/on",
+            "internal initialization jumper off/on",
+            "analog mode jumper off/on",
+            "micro switch off/on",
+        ]
+        b0 = data & 0b00000001
+        b1 = (data & 0b00000010) >> 1
+        b2 = (data & 0b00000100) >> 2
+        b3 = (data & 0b00001000) >> 3
+        b4 = (data & 0b00010000) >> 4
+        b5 = (data & 0b00100000) >> 5
+        b6 = (data & 0b01000000) >> 6
+        b7 = (data & 0b10000000) >> 7
+        data = list(zip([b0, b1, b2, b3, b4, b5, b6, b7], iostatus))
+
+        return data
+
+    @property
+    def alarm_info(self):
+        """
+        Get bits from alarm info register
+        """
+        data = self.query(self._read_commands.get("Alarm info"))
+        data = self._extract_data(data)
+        data = int(data, 16)
+        alarm_info = [
+            "Alarm register 2 contains an error",
+            "Alarm register 1 contains a warning",
+            "Sensor signal < minimum limit",
+            "Sensor signal > maxmum limit",
+            "Batch counter reached its limit",
+            "Power up alarm",
+            "Setpoint-measure too much difference : positive",
+            "Setpoint-measure too much difference : negative",
+            "Master output signal not received or slave factor out of limits",
+            "Hardware alarm, check hardware",
+        ]
+        b0 = data & 0b00000001
+        b1 = (data & 0b00000010) >> 1
+        b2 = (data & 0b00000100) >> 2
+        b3 = (data & 0b00001000) >> 3
+        b4 = (data & 0b00010000) >> 4
+        b5 = (data & 0b00100000) >> 5
+        b5pos = b5 & b2
+        b5neg = b5 & b3
+        b6 = (data & 0b01000000) >> 6
+        b7 = bool((data & 0b10000000) >> 7)
+        data = list(zip([b0, b1, b2, b3, b4, b5, b5pos, b5neg, b6, b7], alarm_info))
+        return data
 
     def _extract_data(self, data):
         """
@@ -235,6 +347,7 @@ class MFC(Instrument):
         Length of the telegram is adjusted with data length.
         data is converted to HEX.
         """
+
         if cmd is None:
             print("no command in the index")
             return
@@ -242,12 +355,20 @@ class MFC(Instrument):
             value_hex = hex(struct.unpack("<I", struct.pack("<f", value))[0])[2:]
         elif isinstance(value, int):
             value_hex = hex(struct.unpack("<I", struct.pack("<I", 16000))[0])[2:]
+        elif isinstance(value, str):
+            value_hex = self._string_to_ascii(value)
         else:
             print("error make pkg")
+
         print(cmd)
         print(value_hex)
         print("type val ", type(value_hex))
         pkg = "".join((cmd, value_hex, "\r\n"))
+
+        # compute and prepend length of the command, especially for string parameters :
+        length = int(len(pkg[:-2]) / 2)
+        # and pad length with '0' to the left
+        pkg = "".join((":", str(length).rjust(2, "0"), pkg))
         print("pkg   , ", pkg)
         return pkg
 
@@ -274,6 +395,14 @@ class MFC(Instrument):
                 )
         else:
             raise ValueError("No data received from the device.")
+
+    # todo not a class function
+    def _string_to_ascii(self, input):
+        """
+        Returns a string of ASCII hex values for an input string,
+        to encode data to send to the device
+        """
+        return "".join([item.encode("utf-8").hex() for item in input])
 
 
 #
